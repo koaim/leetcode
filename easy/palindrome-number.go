ee310ee8c16b352784bd831e831d38e8f0d39eee
Михаил <makar.tula@gmail.com>
2021-08-06 01:16:36 +0300

    Small fixes

--- easy/palindrome-number.go
+++ easy/palindrome-number.go
@@ -2,8 +2,8 @@ package leetcode
 
 /*
 Given an integer x, return true if x is palindrome integer.
-
-An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+An integer is a palindrome when it reads the same backward as forward.
+For example, 121 is palindrome while 123 is not.
 */
 
 func isPalindrome(x int) bool {
