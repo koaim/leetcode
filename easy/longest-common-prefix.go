3d1f1def03aa3cf3d08a23ac995f28713cbc08df
Михаил <makar.tula@gmail.com>
2023-04-16 20:05:26 +0300

    Longest Common Prefix

---
+++ easy/longest-common-prefix.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"strings"
+)
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+*/
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := ""
+	for i := 1; i < len(strs); i++ {
+		for !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+		}
+	}
+
+	return prefix
+}
