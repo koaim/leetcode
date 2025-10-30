67e94e651af9df778697e9fcf132a3161e32afbf
Михаил <makar.tula@gmail.com>
2025-10-30 22:58:40 +0300

    Daily Temperatures

---
+++ medium/739/daily-temperatures.go
@@ -0,0 +1,21 @@
+package leetcode
+
+/*
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
+If there is no future day for which this is possible, keep answer[i] == 0 instead.
+*/
+func dailyTemperatures(temperatures []int) []int {
+	res := make([]int, len(temperatures))
+	stack := []int{}
+
+	for i, v := range temperatures {
+		for len(stack) > 0 && temperatures[stack[len(stack)-1]] < v {
+			j := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res[j] = i - j
+		}
+		stack = append(stack, i)
+	}
+
+	return res
+}
