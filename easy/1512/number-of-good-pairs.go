e37932949570355941bb93813e9ac9ddd0ba053e
Михаил <makar.tula@gmail.com>
2025-11-23 23:07:50 +0300

    Number of Good Pairs

---
+++ easy/1512/number-of-good-pairs.go
@@ -0,0 +1,20 @@
+package leetcode
+
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+*/
+func numIdenticalPairs(nums []int) int {
+	var result int
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				result++
+			}
+		}
+	}
+
+	return result
+}
