3fb8968e9b1c81c06aae7d2c478e3288fe5ea6bb
Михаил <makar.tula@gmail.com>
2025-12-10 00:16:45 +0300

    Valid Palindrome II

--- easy/680/valid-palindrome-ii.go
+++ easy/680/valid-palindrome-ii.go
@@ -6,22 +6,20 @@ Given a string s, return true if the s can be palindrome after deleting at most 
 func validPalindrome(s string) bool {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		if s[i] != s[j] {
-			return isPalindrome(s[:i]+s[i+1:]) || isPalindrome(s[:j]+s[j+1:])
+			return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1)
 		}
 	}
 
 	return true
 }
 
-func isPalindrome(s string) bool {
-	if s == "" {
-		return true
-	}
-
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+func isPalindrome(s string, i, j int) bool {
+	for i < j {
 		if s[i] != s[j] {
 			return false
 		}
+		i++
+		j--
 	}
 
 	return true
