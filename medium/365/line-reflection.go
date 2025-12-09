1910043b8fde43589807c555a107fae586da841d
Михаил <makar.tula@gmail.com>
2025-12-09 20:00:53 +0300

    Line Reflection

---
+++ medium/365/line-reflection.go
@@ -0,0 +1,42 @@
+package leetcode
+
+/*
+Given n points on a 2D plane, find if there is such a line parallel to the y-axis that reflects the given points symmetrically.
+In other words, answer whether or not if there exists a line that after reflecting all points over the given line, the original points' set is the same as the reflected ones.
+
+Note that there can be repeated points.
+*/
+func isReflected(points [][]int) bool {
+	minX, maxX := points[0][0], points[0][0]
+	set := map[[2]int]struct{}{}
+
+	for _, v := range points {
+		x := v[0]
+		y := v[1]
+		if x < minX {
+			minX = x
+		}
+		if x > maxX {
+			maxX = x
+		}
+		set[[2]int{x, y}] = struct{}{}
+	}
+
+	xSim := float64(minX+maxX) / 2.0
+
+	for _, v := range points {
+		reflected := reflectedPoint(v, xSim)
+		if _, ok := set[reflected]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func reflectedPoint(point []int, xSim float64) [2]int {
+	x := int((2 * xSim) - float64(point[0]))
+	y := point[1]
+
+	return [2]int{x, y}
+}
