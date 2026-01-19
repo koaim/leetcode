0b5ce9a2d2b3138f190685c62078d8d37ac69f4d
Михаил <makar.tula@gmail.com>
2026-01-19 03:21:46 +0300

    fix

--- easy/303/range-sum-query-immutable.go
+++ easy/303/range-sum-query-immutable.go
@@ -1,9 +1,5 @@
 package leetcode
 
-type NumArray struct {
-	nums []int
-}
-
 /*
 Given an integer array nums, handle multiple queries of the following type:
   - Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
@@ -12,15 +8,28 @@ Implement the NumArray class:
   - NumArray(int[] nums) Initializes the object with the integer array nums.
   - int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
 */
+type NumArray struct {
+	sum []int
+}
+
 func Constructor(nums []int) NumArray {
-	return NumArray{nums: nums}
+	var sums []int
+	var sum int
+
+	for _, v := range nums {
+		sum += v
+		sums = append(sums, sum)
+	}
+
+	return NumArray{sums}
 }
 
 func (this *NumArray) SumRange(left int, right int) int {
-	var sum int
-	for i := left; i <= right; i++ {
-		sum += this.nums[i]
+	rightV := this.sum[right]
+	if left == 0 {
+		return rightV
 	}
 
-	return sum
+	leftV := this.sum[left-1]
+	return rightV - leftV
 }
--- medium/36/valid-sudoku.go
+++ medium/36/valid-sudoku.go
@@ -7,6 +7,7 @@ Each row must contain the digits 1-9 without repetition.
 Each column must contain the digits 1-9 without repetition.
 Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 */
+
 func isValidSudoku(board [][]byte) bool {
 	lines := [9]map[byte]int{}
 	columns := [9]map[byte]int{}
@@ -25,54 +26,32 @@ func isValidSudoku(board [][]byte) bool {
 
 			if string(value) == "." {
 				continue
+			}
+
+			if val := lines[i][value]; val == 0 {
+				lines[i][value] = 1
 			} else {
-				if val := lines[i][value]; val == 0 {
-					lines[i][value] = 1
-				} else {
-					return false
-				}
+				return false
+			}
 
-				if val := columns[j][value]; val == 0 {
-					columns[j][value] = 1
-				} else {
-					return false
-				}
+			if val := columns[j][value]; val == 0 {
+				columns[j][value] = 1
+			} else {
+				return false
+			}
 
-				box := calcBox(i, j)
-				if _, ok := boxes[box]; !ok {
-					boxes[box] = map[byte]int{}
-				}
+			box := (i/3)*3 + j/3
+			if _, ok := boxes[box]; !ok {
+				boxes[box] = map[byte]int{}
+			}
 
-				if val := boxes[box][value]; val == 0 {
-					boxes[box][value] = 1
-				} else {
-					return false
-				}
+			if val := boxes[box][value]; val == 0 {
+				boxes[box][value] = 1
+			} else {
+				return false
 			}
 		}
 	}
 
 	return true
-}
-
-func calcBox(i, j int) int {
-	if i < 3 {
-		return 3 * (j / 3)
-	} else if i < 6 {
-		if j < 3 {
-			return 1
-		} else if j < 6 {
-			return 4
-		} else {
-			return 7
-		}
-	} else {
-		if j < 3 {
-			return 2
-		} else if j < 6 {
-			return 5
-		} else {
-			return 8
-		}
-	}
 }
