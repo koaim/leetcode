313c67622e5fc3ad9c39a20f1b25c8175e9c4b9c
Михаил <makar.tula@gmail.com>
2026-01-22 02:41:06 +0300

    Find the Prefix Common Array of Two Arrays

---
+++ medium/2657/find-the-prefix-common-array-of-two-arrays.go
@@ -0,0 +1,30 @@
+package leetcode
+
+/*
+You are given two 0-indexed integer permutations A and B of length n.
+A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
+Return the prefix common array of A and B.
+
+A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
+*/
+func findThePrefixCommonArray(A []int, B []int) []int {
+	result := make([]int, len(A))
+	var commonCount int
+	freq := map[int]int{}
+
+	for i := 0; i < len(A); i++ {
+		freq[A[i]]++
+		if freq[A[i]] == 2 {
+			commonCount++
+		}
+
+		freq[B[i]]++
+		if freq[B[i]] == 2 {
+			commonCount++
+		}
+
+		result[i] = commonCount
+	}
+
+	return result
+}
